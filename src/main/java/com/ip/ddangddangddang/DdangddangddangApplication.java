4d0d7fab8bf8ad63ae588f1e160e3c184d38402a
kimpangya <nam74415133@gmail.com>
2024-03-30 03:00:24 +0900

    ✨ feat(user) : main 클래스에 @EnableJpaAuditing @EnableWebSecurity 추가

--- src/main/java/com/ip/ddangddangddang/DdangddangddangApplication.java
+++ src/main/java/com/ip/ddangddangddang/DdangddangddangApplication.java
@@ -2,7 +2,11 @@ package com.ip.ddangddangddang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableJpaAuditing
+@EnableWebSecurity
 @SpringBootApplication
 public class DdangddangddangApplication {
 
