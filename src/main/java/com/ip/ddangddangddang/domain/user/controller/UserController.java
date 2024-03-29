2516d64ec65ecdd534f79ef391580e444d8be11d
kimpangya <nam74415133@gmail.com>
2024-03-30 02:49:17 +0900

    ✨ feat(user) : 회원가입, 로그인 컨트롤러 구현

--- src/main/java/com/ip/ddangddangddang/domain/user/controller/UserController.java
+++ src/main/java/com/ip/ddangddangddang/domain/user/controller/UserController.java
@@ -1,5 +1,18 @@
 package com.ip.ddangddangddang.domain.user.controller;
 
+
+import com.ip.ddangddangddang.domain.user.dto.request.UserSignupRequestDto;
+import com.ip.ddangddangddang.domain.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/version1/users")
+@RestController
 public class UserController {
 
 }
