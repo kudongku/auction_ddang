6ceeed8959a7cbd45bb728abfce5fd1515b532c4
potatobboi <kykdydxo@naver.com>
2024-03-29 15:17:43 +0900

    repository 연결

---
+++ src/main/java/com/ip/ddangddangddang/DdangddangddangApplication.java
@@ -0,0 +1,13 @@
+package com.ip.ddangddangddang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdangddangddangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdangddangddangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ip/ddangddangddang/DdangddangddangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ip.ddangddangddang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdangddangddangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
