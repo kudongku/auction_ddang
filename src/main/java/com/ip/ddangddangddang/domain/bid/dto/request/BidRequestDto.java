0f6f99c333d165fcc20f0ead74eea19815a09b97
potatobboi <kykdydxo@naver.com>
2024-04-05 00:10:54 +0900

    ✨ feat(bid) : 입찰 Dto 구현

--- src/main/java/com/ip/ddangddangddang/domain/bid/dto/request/BidRequestDto.java
+++ src/main/java/com/ip/ddangddangddang/domain/bid/dto/request/BidRequestDto.java
@@ -1,5 +1,12 @@
 package com.ip.ddangddangddang.domain.bid.dto.request;
 
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+
+@Getter
 public class BidRequestDto {
+
+    @Positive(message = "음수나, 공백이 올 수 없습니다.")
+    private Long price;
 
 }
