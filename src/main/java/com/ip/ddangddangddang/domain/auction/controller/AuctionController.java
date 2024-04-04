fa8626db55daf1c8caac224f5fcad44c79ecb51d
kimpangya <nam74415133@gmail.com>
2024-04-04 17:01:52 +0900

    ✨ feat(auction) : 옥션 생성 컨트롤러 구현

--- src/main/java/com/ip/ddangddangddang/domain/auction/controller/AuctionController.java
+++ src/main/java/com/ip/ddangddangddang/domain/auction/controller/AuctionController.java
@@ -1,5 +1,30 @@
 package com.ip.ddangddangddang.domain.auction.controller;
 
+
+import com.ip.ddangddangddang.domain.auction.dto.request.AuctionRequestDto;
+import com.ip.ddangddangddang.domain.auction.service.AuctionService;
+import com.ip.ddangddangddang.global.security.UserDetailsImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/v1/auctions")
+@RestController
 public class AuctionController {
+
+    private final AuctionService auctionService;
+
+    @PostMapping
+    public void createAuction(
+        @Valid @RequestBody AuctionRequestDto requestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        auctionService.createAuction(requestDto, userDetails.getUser());
+    }
 
 }
