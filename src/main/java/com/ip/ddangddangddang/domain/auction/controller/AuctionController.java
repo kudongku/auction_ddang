5da7ea0daa57650b7cfcf1590c87debef74606da
kudongku <kudongku@naver.com>
2024-04-05 11:23:26 +0900

    :sparkles: feat(S3) : Auction controller, service에 S3관련 추가

--- src/main/java/com/ip/ddangddangddang/domain/auction/controller/AuctionController.java
+++ src/main/java/com/ip/ddangddangddang/domain/auction/controller/AuctionController.java
@@ -2,17 +2,21 @@ package com.ip.ddangddangddang.domain.auction.controller;
 
 
 import com.ip.ddangddangddang.domain.auction.dto.request.AuctionRequestDto;
+import com.ip.ddangddangddang.domain.auction.dto.response.AuctionResponseDto;
 import com.ip.ddangddangddang.domain.auction.service.AuctionService;
 import com.ip.ddangddangddang.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RequestMapping("/v1/auctions")
@@ -21,12 +25,20 @@ public class AuctionController {
 
     private final AuctionService auctionService;
 
+    @GetMapping("/{auctionId}")
+    public AuctionResponseDto getAuction(
+        @PathVariable Long auctionId
+    ) {
+        return auctionService.getAuction(auctionId);
+    }
+
     @PostMapping
     public void createAuction(
-        @Valid @RequestBody AuctionRequestDto requestDto,
+        @RequestPart("auctionImage") MultipartFile auctionImage,
+        @Valid @RequestPart("requestDto") AuctionRequestDto requestDto,
         @AuthenticationPrincipal UserDetailsImpl userDetails
-    ) {
-        auctionService.createAuction(requestDto, userDetails.getUser());
+    ) throws IOException {
+        auctionService.createAuction(auctionImage, requestDto, userDetails.getUser());
     }
 
     @DeleteMapping("/{auctionId}")
--- src/main/java/com/ip/ddangddangddang/domain/auction/service/AuctionService.java
+++ src/main/java/com/ip/ddangddangddang/domain/auction/service/AuctionService.java
@@ -1,29 +1,43 @@
 package com.ip.ddangddangddang.domain.auction.service;
 
 import com.ip.ddangddangddang.domain.auction.dto.request.AuctionRequestDto;
+import com.ip.ddangddangddang.domain.auction.dto.response.AuctionResponseDto;
 import com.ip.ddangddangddang.domain.auction.entity.Auction;
+import com.ip.ddangddangddang.domain.auction.entity.File;
 import com.ip.ddangddangddang.domain.auction.repository.AuctionRepository;
 import com.ip.ddangddangddang.domain.user.entity.User;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class AuctionService {
 
+    private final FileService fileService;
     private final AuctionRepository auctionRepository;
 
     @Transactional
-    public void createAuction(AuctionRequestDto requestDto, User user) {
-        auctionRepository.save(new Auction(requestDto, user));
+    public void createAuction(MultipartFile auctionImage, AuctionRequestDto requestDto, User user)
+        throws IOException {
+        File file = fileService.upload(auctionImage, requestDto.getObjectName());
+        auctionRepository.save(new Auction(requestDto, user, file));
     }
 
     @Transactional
     public void deleteAuction(Long auctionId, User user) {
-
+        Auction auction = auctionRepository.findById(auctionId);
+        fileService.delete(auction.getFileKeyName());
         auctionRepository.delete(auctionId, user.getId());
     }
 
+    @Transactional(readOnly = true)
+    public AuctionResponseDto getAuction(Long auctionId) {
+        Auction auction = auctionRepository.findById(auctionId);
+        String preSignedURL = fileService.getPresignedURL(auction.getFileKeyName());
+        return new AuctionResponseDto(auction, preSignedURL);
+    }
 }
