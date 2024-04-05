7945821f598b4d7f7e2366c822763e88cf10419a
kudongku <kudongku@naver.com>
2024-04-05 11:16:34 +0900

    :recycle: refactor(S3) : S3Service 리팩터링
    - objectMetadata에 컨텐트 타입 설정
    - delete, getURL에서 try catch문 삭제

--- src/main/java/com/ip/ddangddangddang/domain/auction/service/S3Service.java
+++ src/main/java/com/ip/ddangddangddang/domain/auction/service/S3Service.java
@@ -1,6 +1,5 @@
 package com.ip.ddangddangddang.domain.auction.service;
 
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
@@ -11,12 +10,10 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-@Slf4j
 @RequiredArgsConstructor
 @Service
 public class S3Service {
@@ -27,7 +24,9 @@ public class S3Service {
 
     public String upload(MultipartFile multipartFile, String keyName) throws IOException {
         ObjectMetadata objMeta = new ObjectMetadata();
+        objMeta.setContentType(multipartFile.getContentType());
         objMeta.setContentLength(multipartFile.getInputStream().available());
+
         amazonS3.putObject(bucket, keyName, multipartFile.getInputStream(), objMeta);
         return URLDecoder.decode(
             amazonS3.getUrl(bucket, keyName).toString(),
@@ -36,32 +35,21 @@ public class S3Service {
     }
 
     public void delete(String keyName) {
-        try {
-            amazonS3.deleteObject(bucket, keyName);
-        } catch (AmazonServiceException e) {
-            log.error(e.toString());
-        }
+        amazonS3.deleteObject(bucket, keyName);
     }
 
     public String getPresignedURL(String keyName) {
-        String preSignedURL = "";
         Date expiration = new Date();
-        Long expTimeMillis = expiration.getTime();
+        long expTimeMillis = expiration.getTime();
         expTimeMillis += 1000 * 60 * 2;
         expiration.setTime(expTimeMillis);
 
-        try {
-            GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(
-                bucket, keyName)
-                .withMethod(HttpMethod.GET)
-                .withExpiration(expiration);
-            URL url = amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
-            preSignedURL = url.toString();
-        } catch (Exception e) {
-            log.error(e.toString());
-        }
-
-        return preSignedURL;
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(
+            bucket, keyName)
+            .withMethod(HttpMethod.GET)
+            .withExpiration(expiration);
+        URL url = amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
+        return url.toString();
     }
 
 }
