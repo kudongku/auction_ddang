03f88adc9abe4ca9b44c3eb6fd3f67ace4198d9b
potatobboi <kykdydxo@naver.com>
2024-04-06 22:21:29 +0900

    ✨ feat(global) : healthCheck 메서드 구현

--- src/main/java/com/ip/ddangddangddang/global/config/WebSecurityConfig.java
+++ src/main/java/com/ip/ddangddangddang/global/config/WebSecurityConfig.java
@@ -71,6 +71,7 @@ public class WebSecurityConfig {
                 .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
                 .requestMatchers("/v1/users/signin").permitAll() // 회원가입,로그인 요청 모두 접근 허가
                 .requestMatchers("/v1/users/signup").permitAll()
+                .requestMatchers("/health").permitAll()
                 .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
