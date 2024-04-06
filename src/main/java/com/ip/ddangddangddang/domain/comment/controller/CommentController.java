4b444337659cc1f43af1efa11393d0f5a1fd7cca
potatobboi <kykdydxo@naver.com>
2024-04-07 00:17:31 +0900

    ✨ feat(comment) : Comment 작성 컨트롤러 구현

--- src/main/java/com/ip/ddangddangddang/domain/comment/controller/CommentController.java
+++ src/main/java/com/ip/ddangddangddang/domain/comment/controller/CommentController.java
@@ -1,5 +1,30 @@
 package com.ip.ddangddangddang.domain.comment.controller;
 
+import com.ip.ddangddangddang.domain.comment.dto.request.CommentRequestDto;
+import com.ip.ddangddangddang.domain.comment.service.CommentService;
+import com.ip.ddangddangddang.global.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/v1/auctions/{auctionId}/comments")
+@RestController
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public void createComment(
+        @PathVariable Long auctionId,
+        @RequestBody CommentRequestDto requestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        commentService.createComment(auctionId, requestDto, userDetails.getUser().getId());
+    }
 
 }
