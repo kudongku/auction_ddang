8fa1501fecc8072549369feeee3dd1b979bfe3c0
si eun <tldms123455@naver.com>
2024-04-06 16:18:44 +0900

    :sparkles: : feat(global) : GlobalExceptionHandler 및 CustomException 추가

---
+++ src/main/java/com/ip/ddangddangddang/global/exception/custom/TimeOutLockException.java
@@ -0,0 +1,9 @@
+package com.ip.ddangddangddang.global.exception.custom;
+
+public class TimeOutLockException extends RuntimeException {
+
+    public TimeOutLockException(final String message) {
+        super(message);
+    }
+
+}
