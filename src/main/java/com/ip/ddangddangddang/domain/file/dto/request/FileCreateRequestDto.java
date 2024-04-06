378bb75d7dda34a7d19f0e9b28f63d72283a939e
kudongku <kudongku@naver.com>
2024-04-06 22:53:08 +0900

    :recycle: refactor(file) : return type을 dto로 변경

--- src/main/java/com/ip/ddangddangddang/domain/file/dto/request/FileCreateRequestDto.java
+++ src/main/java/com/ip/ddangddangddang/domain/file/dto/request/FileCreateRequestDto.java
@@ -1,10 +1,12 @@
 package com.ip.ddangddangddang.domain.file.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class ImageCreateRequestDto {
+@AllArgsConstructor
+public class FileCreateRequestDto {
     private String imageName;
 }
