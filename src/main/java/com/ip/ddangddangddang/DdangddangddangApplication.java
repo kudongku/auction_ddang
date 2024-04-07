e2a199658499d2f7d6367b7edd099c14d75c2c0f
si eun <tldms123455@naver.com>
2024-04-07 14:10:52 +0900

    :wrench: chore(global) : queryDsl을 위한 세팅

--- src/main/java/com/ip/ddangddangddang/DdangddangddangApplication.java
+++ src/main/java/com/ip/ddangddangddang/DdangddangddangApplication.java
@@ -2,10 +2,8 @@ package com.ip.ddangddangddang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableJpaAuditing
 @EnableWebSecurity
 @SpringBootApplication
 public class DdangddangddangApplication {
