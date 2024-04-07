32c9713b28c8f47df7e791ae14c4b44de8fc015d
potatobboi <kykdydxo@naver.com>
2024-04-07 18:19:38 +0900

    ✨ feat(comment) : 댓글 조회 비즈니스 로직 구현
    - validateUser 메서드 || -> && 로 변경

--- src/main/java/com/ip/ddangddangddang/domain/comment/service/CommentService.java
+++ src/main/java/com/ip/ddangddangddang/domain/comment/service/CommentService.java
@@ -3,6 +3,7 @@ package com.ip.ddangddangddang.domain.comment.service;
 import com.ip.ddangddangddang.domain.auction.entity.Auction;
 import com.ip.ddangddangddang.domain.auction.service.AuctionService;
 import com.ip.ddangddangddang.domain.comment.dto.request.CommentCreateRequestDto;
+import com.ip.ddangddangddang.domain.comment.dto.response.CommentReadResponseDto;
 import com.ip.ddangddangddang.domain.comment.entity.Comment;
 import com.ip.ddangddangddang.domain.comment.repository.CommentRepository;
 import com.ip.ddangddangddang.domain.result.entity.Result;
@@ -10,6 +11,7 @@ import com.ip.ddangddangddang.domain.result.service.ResultService;
 import com.ip.ddangddangddang.domain.user.entity.User;
 import com.ip.ddangddangddang.domain.user.service.UserService;
 import com.ip.ddangddangddang.global.exception.custom.CustomUserException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +28,7 @@ public class CommentService {
 
     @Transactional
     public void createComment(Long auctionId, CommentCreateRequestDto requestDto, Long userId) {
-        User user = userService.getUser(userId);
+        User user = userService.findUserOrElseThrow(userId);
         Auction auction = auctionService.findAuctionOrElseThrow(auctionId);
         Result result = resultService.findResultOrElseThrow(auctionId);
 
@@ -36,6 +38,21 @@ public class CommentService {
         validateUser(sellerId, buyerId, userId);
         Comment comment = new Comment(requestDto.getContent(), user, auction);
         commentRepository.save(comment);
+    }
+
+    public List<CommentReadResponseDto> getComments(Long auctionId, Long userId) {
+        Auction auction = auctionService.findAuctionOrElseThrow(auctionId);
+        Result result = resultService.findResultOrElseThrow(auctionId);
+
+        Long sellerId = auction.getUser().getId();
+        Long buyerId = result.getUser().getId();
+
+        validateUser(sellerId, buyerId, userId);
+
+        List<Comment> comments = commentRepository.findAllByAuctionIdOrderByCreatedAt(auctionId);
+        return comments.stream()
+            .map(CommentReadResponseDto::new)
+            .toList();
     }
 
     private boolean isSellerOfAuction(Long sellerId, Long userId) {
@@ -48,8 +65,8 @@ public class CommentService {
 
     private void validateUser(Long sellerId, Long buyerId, Long userId) {
         if (!isSellerOfAuction(sellerId, userId)
-            || !isBuyerOfAuction(buyerId, userId)) {
-            throw new CustomUserException("댓글 작성 권한이 없습니다.");
+            && !isBuyerOfAuction(buyerId, userId)) {
+            throw new CustomUserException("댓글 권한이 없습니다.");
         }
     }
 
