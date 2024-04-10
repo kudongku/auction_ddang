7f57e32446249ff818629cfc32da1f6874acb93e
potatobboi <kykdydxo@naver.com>
2024-04-10 20:51:20 +0900

    ♻️ refactor(user) : 로그인 실패 시 에러메세지 수정 및 서비스 메서드 수정

---
+++ src/main/java/com/ip/ddangddangddang/global/exception/custom/UserEmailAlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.ip.ddangddangddang.global.exception.custom;
+
+public class UserEmailAlreadyExistException extends RuntimeException {
+
+    public UserEmailAlreadyExistException(String message) {
+        super(message);
+    }
+
+}
