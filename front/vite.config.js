6fa776fac72355e197f4a5d8a0b98c39c05f1ede
kimpangya <nam74415133@gmail.com>
2024-04-11 20:48:23 +0900

    🐛 fix(front) : CORS 에러 해결

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: [{find: "@", replacement: "/src"}],
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
 });
