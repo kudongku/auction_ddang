e49817c361f8eaeec3be0b6a9d48186130c48be7
si eun <tldms123455@naver.com>
2024-04-13 00:25:02 +0900

    :recycle: fix(auction) : page -> slice로 쿼리 최적화 & 무한스크롤 조건으로 변경

--- src/main/java/com/ip/ddangddangddang/domain/auction/controller/AuctionController.java
+++ src/main/java/com/ip/ddangddangddang/domain/auction/controller/AuctionController.java
@@ -4,18 +4,20 @@ package com.ip.ddangddangddang.domain.auction.controller;
 import com.ip.ddangddangddang.domain.auction.dto.request.AuctionRequestDto;
 import com.ip.ddangddangddang.domain.auction.dto.response.AuctionListResponseDto;
 import com.ip.ddangddangddang.domain.auction.dto.response.AuctionResponseDto;
+import com.ip.ddangddangddang.domain.auction.entity.StatusEnum;
 import com.ip.ddangddangddang.domain.auction.service.AuctionService;
+import com.ip.ddangddangddang.domain.common.dto.Response;
 import com.ip.ddangddangddang.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,11 +47,14 @@ public class AuctionController {
     }
 
     @GetMapping
-    public Page<AuctionListResponseDto> getAuctions(
+    public Response<Slice<AuctionListResponseDto>> getAuctions(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
-        Pageable pageable
+        Pageable pageable,
+        @RequestParam(required = false) StatusEnum status,
+        @RequestParam(required = false) String title
     ) {
-        return auctionService.getAuctions(userDetails.getUserId(), pageable);
+        return Response.ok(
+            auctionService.getAuctions(userDetails.getUserId(), status, title, pageable));
     }
 
     @GetMapping("/{auctionId}")
@@ -59,7 +64,7 @@ public class AuctionController {
         return auctionService.getAuction(auctionId);
     }
 
-    @PutMapping("/{auctionId}")
+    @PatchMapping("/{auctionId}")
     public void completeAuction(
         @PathVariable Long auctionId,
         @AuthenticationPrincipal UserDetailsImpl userDetails
@@ -67,16 +72,8 @@ public class AuctionController {
         auctionService.updateStatusToComplete(auctionId, userDetails.getUserId());
     }
 
-    @GetMapping("/search")
-    public Page<AuctionListResponseDto> searchAuctions(
-        @RequestParam String title,
-        Pageable pageable
-    ) {
-        return auctionService.getAuctionsByTitle(title, pageable);
-    }
-
     @GetMapping("/myauctions")
-    public Page<AuctionListResponseDto> getMyAuctions(
+    public Slice<AuctionListResponseDto> getMyAuctions(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         Pageable pageable
     ) {
@@ -84,7 +81,7 @@ public class AuctionController {
     }
 
     @GetMapping("/mybids")
-    public Page<AuctionListResponseDto> getMyBids(
+    public Slice<AuctionListResponseDto> getMyBids(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         Pageable pageable
     ) {
