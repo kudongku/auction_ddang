f2df504f5ec3ad012a9fbe35307792e38663bac4
kudongku <kudongku@naver.com>
2024-04-13 19:42:12 +0900

    🐛fix(front) : 네비게이션 바의 햄버거 메뉴바 제거

--- front/src/widgets/layout/dashboard-navbar.jsx
+++ front/src/widgets/layout/dashboard-navbar.jsx
@@ -1,44 +1,39 @@
-import { useLocation, Link } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom';
 import {
-  Navbar,
-  Typography,
+  Avatar,
+  Breadcrumbs,
   Button,
   IconButton,
-  Breadcrumbs,
   Input,
   Menu,
   MenuHandler,
-  MenuList,
   MenuItem,
-  Avatar,
-} from "@material-tailwind/react";
+  MenuList,
+  Navbar,
+  Typography,
+} from '@material-tailwind/react';
 import {
-  UserCircleIcon,
-  Cog6ToothIcon,
   BellIcon,
   ClockIcon,
+  Cog6ToothIcon,
   CreditCardIcon,
-  Bars3Icon,
-} from "@heroicons/react/24/solid";
-import {
-  useMaterialTailwindController,
-  setOpenConfigurator,
-  setOpenSidenav,
-} from "@/context";
+  UserCircleIcon,
+} from '@heroicons/react/24/solid';
+import { setOpenConfigurator, useMaterialTailwindController } from '@/context';
 
 export function DashboardNavbar() {
   const [controller, dispatch] = useMaterialTailwindController();
   const { fixedNavbar, openSidenav } = controller;
   const { pathname } = useLocation();
-  const [layout, page] = pathname.split("/").filter((el) => el !== "");
+  const [layout, page] = pathname.split('/').filter((el) => el !== '');
 
   return (
     <Navbar
-      color={fixedNavbar ? "white" : "transparent"}
+      color={fixedNavbar ? 'white' : 'transparent'}
       className={`rounded-xl transition-all ${
         fixedNavbar
-          ? "sticky top-4 z-40 py-3 shadow-md shadow-blue-gray-500/5"
-          : "px-0 py-1"
+          ? 'sticky top-4 z-40 py-3 shadow-md shadow-blue-gray-500/5'
+          : 'px-0 py-1'
       }`}
       fullWidth
       blurred={fixedNavbar}
@@ -47,7 +42,7 @@ export function DashboardNavbar() {
         <div className="capitalize">
           <Breadcrumbs
             className={`bg-transparent p-0 transition-all ${
-              fixedNavbar ? "mt-1" : ""
+              fixedNavbar ? 'mt-1' : ''
             }`}
           >
             <Link to={`/${layout}`}>
@@ -75,19 +70,19 @@ export function DashboardNavbar() {
           <div className="mr-auto md:mr-4 md:w-56">
             <Input label="Search" />
           </div>
-          <IconButton
-            variant="text"
-            color="blue-gray"
-            className="grid xl:hidden"
-            onClick={() => setOpenSidenav(dispatch, !openSidenav)}
-          >
-            <Bars3Icon strokeWidth={3} className="h-6 w-6 text-blue-gray-500" />
-          </IconButton>
+          {/*<IconButton*/}
+          {/*  variant="text"*/}
+          {/*  color="blue-gray"*/}
+          {/*  className="grid xl:hidden"*/}
+          {/*  onClick={() => setOpenSidenav(dispatch, !openSidenav)}*/}
+          {/*>*/}
+          {/*  <Bars3Icon strokeWidth={3} className="h-6 w-6 text-blue-gray-500" />*/}
+          {/*</IconButton>*/}
           <Link to="/auth/sign-in">
             <Button
               variant="text"
               color="blue-gray"
-              className="hidden items-center gap-1 px-4 xl:flex normal-case"
+              className="hidden items-center gap-1 px-4 normal-case xl:flex"
             >
               <UserCircleIcon className="h-5 w-5 text-blue-gray-500" />
               Sign In
@@ -191,6 +186,6 @@ export function DashboardNavbar() {
   );
 }
 
-DashboardNavbar.displayName = "/src/widgets/layout/dashboard-navbar.jsx";
+DashboardNavbar.displayName = '/src/widgets/layout/dashboard-navbar.jsx';
 
 export default DashboardNavbar;
