fbe1ad10dccc9edb051a5fc198ae4e3ee080bb28
kudongku <kudongku@naver.com>
2024-04-13 18:36:51 +0900

    🐛fix(front) : `vite.config.js` proxy 해제

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,13 +6,4 @@ export default defineConfig({
   resolve: {
     alias: [{find: "@", replacement: "/src"}],
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
-  }
 });
