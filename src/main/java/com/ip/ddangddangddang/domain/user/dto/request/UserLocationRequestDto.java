2a5b7d7ffda1d9560d564532e03c21a879475cfb
potatobboi <kykdydxo@naver.com>
2024-04-21 21:45:09 +0900

    ♻️ refactor(user) : Builder 추가

--- src/main/java/com/ip/ddangddangddang/domain/user/dto/request/UserLocationRequestDto.java
+++ src/main/java/com/ip/ddangddangddang/domain/user/dto/request/UserLocationRequestDto.java
@@ -1,9 +1,11 @@
 package com.ip.ddangddangddang.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class UserLocationRequestDto {
 
     @NotBlank
