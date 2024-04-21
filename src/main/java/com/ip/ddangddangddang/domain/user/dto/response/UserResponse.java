ed9d3c7f1e1bf9d8a93350dc03e98ab964d36233
potatobboi <kykdydxo@naver.com>
2024-04-22 01:44:00 +0900

    ♻️ refactor(user) : 빌더 추가

--- src/main/java/com/ip/ddangddangddang/domain/user/dto/response/UserResponse.java
+++ src/main/java/com/ip/ddangddangddang/domain/user/dto/response/UserResponse.java
@@ -1,8 +1,10 @@
 package com.ip.ddangddangddang.domain.user.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class UserResponse {
 
     private final Long townId;
