dbd73f9bf2709b6f3cf07d0f155f97679297cf4f
potatobboi <kykdydxo@naver.com>
2024-04-22 01:43:05 +0900

    ♻️ refactor(user) : 닉네임 입력값 패턴 제거

--- src/main/java/com/ip/ddangddangddang/domain/user/dto/request/UserUpdateRequestDto.java
+++ src/main/java/com/ip/ddangddangddang/domain/user/dto/request/UserUpdateRequestDto.java
@@ -1,7 +1,6 @@
 package com.ip.ddangddangddang.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,10 +9,6 @@ import lombok.Getter;
 public class UserUpdateRequestDto {
 
     @NotBlank(message = "닉네임은 필수로 입력해야 합니다.")
-    @Pattern(
-        regexp = "^(?!\\d+$)[a-zA-Z가-힣\\d]{2,10}$",
-        message = "닉네임은 2~10자로 구성되어야 하며, 숫자로만 구성될 수 없습니다."
-    )
     private String nickname;
 
     @NotBlank(message = "비밀번호는 필수로 입력해야 합니다.")
