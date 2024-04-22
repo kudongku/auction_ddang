aaa0ef1cd7f4768de44a9463ee45ac15fe797449
kimpangya <nam74415133@gmail.com>
2024-04-22 17:27:05 +0900

    ♻️ refactor(comment) : 어노테이션 추가

--- src/main/java/com/ip/ddangddangddang/domain/comment/dto/request/CommentCreateRequestDto.java
+++ src/main/java/com/ip/ddangddangddang/domain/comment/dto/request/CommentCreateRequestDto.java
@@ -2,8 +2,10 @@ package com.ip.ddangddangddang.domain.comment.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CommentCreateRequestDto {
 
