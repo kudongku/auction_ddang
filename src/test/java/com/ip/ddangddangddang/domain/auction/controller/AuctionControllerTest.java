2304ed4e7fa80780e3849c5c67be0cb04d499b09
si eun <tldms123455@naver.com>
2024-04-22 11:38:21 +0900

    🧪 test(auction) : auction 전체 조회 테스트 추가

--- src/test/java/com/ip/ddangddangddang/domain/auction/controller/AuctionControllerTest.java
+++ src/test/java/com/ip/ddangddangddang/domain/auction/controller/AuctionControllerTest.java
@@ -5,6 +5,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -53,6 +54,21 @@ class AuctionControllerTest extends ControllerTest implements AuctionServiceTest
         @Test
         void 옥션_삭제_테스트() throws Exception {
             mockMvc.perform(delete("/v1/auctions/{auctionId}", TEST_TOWN1_AUCTION1_ID))
+                .andExpect(status().isOk())
+                .andDo(print());
+        }
+    }
+
+    @Nested
+    @DisplayName("옥션 전체 조회 테스트")
+    class GetAuctionsTest {
+
+        @Test
+        void 옥션_전체_조회_테스트() throws Exception {
+            mockMvc.perform(get("/v1/auctions")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .param("page", String.valueOf(PAGE_NUMBER))
+                    .param("size", String.valueOf(PAGE_SIZE)))
                 .andExpect(status().isOk())
                 .andDo(print());
         }
