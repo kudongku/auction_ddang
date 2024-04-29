8a4096f75517c2457304a1d97278276a688a3b96
kudongku <kudongku@naver.com>
2024-04-29 20:15:09 +0900

    :recycle: refactor(global) : exception handler의 dto에서 error종류 삭제

--- src/main/java/com/ip/ddangddangddang/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ip/ddangddangddang/global/exception/GlobalExceptionHandler.java
@@ -29,109 +29,94 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(TimeOutLockException.class)
     public ResponseEntity<ExceptionDto> timeOutLockException(final TimeOutLockException e) {
-        log.error("TimeOutLockException: ", e);
-        return createResponse(HttpStatus.REQUEST_TIMEOUT, "Lock Exception: " + e.getMessage());
+        log.error(e.getMessage());
+        return createResponse(HttpStatus.REQUEST_TIMEOUT, e.getMessage());
     }
 
     @ExceptionHandler(LockNotAcquiredException.class)
     public ResponseEntity<ExceptionDto> lockNotAcquiredException(final LockNotAcquiredException e) {
         log.error("LockNotAcquiredException: ", e);
-        return createResponse(HttpStatus.REQUEST_TIMEOUT, "Lock Exception: " + e.getMessage());
+        return createResponse(HttpStatus.REQUEST_TIMEOUT, e.getMessage());
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ExceptionDto> entityNotFoundException(final EntityNotFoundException e) {
         log.error("EntityNotFoundException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST, "EntityNotFoundException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST,  e.getMessage());
     }
 
     @ExceptionHandler(CustomAuctionException.class)
     public ResponseEntity<ExceptionDto> customAuctionException(final CustomAuctionException e) {
         log.error("CustomAuctionException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST,
-            "CustomAuctionException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(CustomBidException.class)
     public ResponseEntity<ExceptionDto> customBidException(final CustomBidException e) {
         log.error("CustomBidException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST, "CustomBidException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(CustomCommentException.class)
     public ResponseEntity<ExceptionDto> customCommentException(final CustomCommentException e) {
         log.error("CustomCommentException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST, "CustomCommentException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(CustomResultException.class)
     public ResponseEntity<ExceptionDto> customResultException(final CustomResultException e) {
         log.error("CustomResultException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST, "CustomResultException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(CustomTownException.class)
     public ResponseEntity<ExceptionDto> customTownException(final CustomTownException e) {
         log.error("CustomTownException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST, "CustomTownException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(CustomUserException.class)
     public ResponseEntity<ExceptionDto> customUserException(final CustomUserException e) {
         log.error("CustomUserException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST, "CustomUserException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ExceptionDto> userNotFoundException(final UserNotFoundException e) {
         log.error("UserNotFoundException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST, "UserNotFoundException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(FileNotFoundException.class)
     public ResponseEntity<ExceptionDto> fileNotFoundException(final FileNotFoundException e) {
         log.error("FileNotFoundException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST, "FileNotFoundException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST,  e.getMessage());
     }
 
     @ExceptionHandler(UserHasNotAuthorityToFileException.class)
     public ResponseEntity<ExceptionDto> userHasNoAuthorityException(
         final UserHasNotAuthorityToFileException e) {
         log.error("UserHasNoAuthorityException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST,
-            "UserHasNoAuthorityException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST,e.getMessage());
     }
 
     @ExceptionHandler(UserHasNotAuthorityToAuctionException.class)
     public ResponseEntity<ExceptionDto> userHasNotAuthorityToAuctionException(
         final UserHasNotAuthorityToAuctionException e) {
         log.error("UserHasNotAuthorityToAuctionException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST,
-            "UserHasNotAuthorityToAuctionException: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(AuctionNotFoundException.class)
     public ResponseEntity<ExceptionDto> auctionNotFoundException(final AuctionNotFoundException e) {
         log.error("AuctionNotFoundException: ", e);
-        return createResponse(HttpStatus.BAD_REQUEST,
-            "AuctionNotFoundException: " + e.getMessage());
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionDto> handleMethodArgumentNotValidException(
-        final MethodArgumentNotValidException e
-    ) {
-        log.error("MethodArgumentNotValidException: ", e);
-        return createResponse(
-            HttpStatus.OK,
-            Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage()
-        );
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     ResponseEntity<ExceptionDto> exception(final Exception e) {
         log.error("Exception: ", e);
-        return createResponse(HttpStatus.I_AM_A_TEAPOT, "Unknown Error: " + e.getMessage());
+        return createResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     private ResponseEntity<ExceptionDto> createResponse(
